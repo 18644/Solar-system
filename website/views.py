519a6528aca7c93a3883b8f94bb4d3ae88a243ab
Negin <80224111+18644@users.noreply.github.com>
2022-05-18 10:21:36 +1200

    creating routes and their respective html.

--- website/views.py
+++ website/views.py
@@ -1,16 +1,25 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
+from sqlalchemy import func
+from .models import SolarSystem, Planet
 from . import db
-from website.models import solarSystem, planet
-
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET'])
+@views.route('/', methods=['POST']) 
+@login_required
 def home():
-    if request.method == 'GET':
-    solarSystem = request.form.get('solarSystem')
-    
-    return render_template('home.html')
+        Search = request.form.get('SolarSystem')
+        return render_template("home.html", user=current_user)
+
+@views.route('/planet', methods=['POST', 'GET']) 
+@login_required
+def planet():
+        if request.method == "POST":
+                Planet = request.form.get('Planet')
+                
+                
+        return render_template("home.html", Planet, user=current_user)
 
 
 
