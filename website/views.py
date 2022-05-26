61d5c477d1bc9e0990f90acaaca5674b036092d8
Negin <80224111+18644@users.noreply.github.com>
2022-05-27 11:49:03 +1200

    adding sign up, login and fixing nav bar links.

--- website/views.py
+++ website/views.py
@@ -1,25 +1,20 @@
 from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
-from sqlalchemy import func
 from .models import SolarSystem, Planet
 from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['POST']) 
+@views.route('/SolarSystems', methods=['GET', 'POST']) 
 @login_required
-def home():
-        Search = request.form.get('SolarSystem')
-        return render_template("home.html", user=current_user)
+def SolarSystems():
+        if request.method == "GET":
+                query = SolarSystem.query.all()
+                return render_template("home.html", query ,user = current_user)
 
 @views.route('/planet', methods=['POST', 'GET']) 
 @login_required
 def planet():
-        if request.method == "POST":
-                Planet = request.form.get('Planet')
-                
-                
-        return render_template("home.html", Planet, user=current_user)
-
-
-
+        if request.method == "GET":
+                query = Planet.query.all()
+                return render_template("planet.html", query ,user=current_user)
